783759ef449e917ee1335879097bd821da03b7dc
mohamed-magdy-1 <King44123no@gamil.com>
2025-09-29 07:02:47 +0300

    fix

--- config/server.js
+++ config/server.js
@@ -1,23 +1,49 @@
+// module.exports = ({ env }) => ({
+//   host: '0.0.0.0',
+//   port: env.int('PORT', 1337),
+//   proxy: true,
+//   url: env('PUBLIC_URL', 'https://io-tech-task-back-end.onrender.com/'),
+  
+//   app: {
+//     keys: [
+//       env('APP_KEY_1', 'someRandomKey1'),
+//       env('APP_KEY_2', 'someRandomKey2'),
+//       env('APP_KEY_3', 'someRandomKey3'),
+//       env('APP_KEY_4', 'someRandomKey4'),
+//     ],
+//   },
 
-
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
+//       session: {
+//         secure: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
-  host: '0.0.0.0', // مهم عشان Render يقدر يشوف السيرفر
-  port: env.int('PORT', 1337), // Render هيبعت PORT=10000 غالباً
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env('PUBLIC_URL', 'http://localhost:1337/'),
+
   app: {
-    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  cors: {
-    enabled: true,
-    origin: [
-      'https://swordmanhwa.site',
-      'https://www.swordmanhwa.site',
+    keys: [
+      env('APP_KEY_1', 'fallback_key_1'),
+      env('APP_KEY_2', 'fallback_key_2'),
+      env('APP_KEY_3', 'fallback_key_3'),
+      env('APP_KEY_4', 'fallback_key_4'),
     ],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
+  },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
+      session: {
+      
+        secure: false,
+      },
+    },
   },
 });
-
