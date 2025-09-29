49cca5f2850b8b558b2cac9abbebacdfb841c71b
mohamed-magdy-1 <King44123no@gamil.com>
2025-09-29 05:32:56 +0300

    new dp

--- config/database.js
+++ config/database.js
@@ -1,47 +1,50 @@
-const fs = require('fs');
 const path = require('path');
-
-console.log('CA path:', path.join(__dirname, 'certs/ca.pem'));
-console.log('Exists:', fs.existsSync(path.join(__dirname, 'certs/ca.pem')));
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
-  const useSSL = env.bool('DATABASE_SSL', false);
-  const useCA = env.bool('DATABASE_USE_CA', false);
+  const sslConfig = env.bool('DATABASE_SSL', false)
+    ? { rejectUnauthorized: false }
+    : false;
 
-let ssl = false;
-if (useSSL) {
-  ssl = useCA
-    ? {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(path.join(__dirname, 'certs/ca.pem'), 'utf8'),
-      }
-    : { rejectUnauthorized: false };
-}
-
-
-  const connection =
-    client === 'sqlite'
-      ? {
-          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        }
-      : {
-          connectionString: env('DATABASE_URL'),
-          ssl,
-          schema: env('DATABASE_SCHEMA', 'public'),
-        };
+  const connections = {
+    mysql: {
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: sslConfig,
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: sslConfig,
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  };
 
   return {
     connection: {
       client,
-      connection,
+      ...connections[client],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
     },
   };
 };
-
